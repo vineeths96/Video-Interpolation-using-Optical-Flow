2d684fddffc99ec37dc9f3ee75dd80bf8798b985
vineeths96 <vinsree1996@gmail.com>
2020-11-15 21:12:29 +0530

    Minor fix

--- problem_2/sphere.py
+++ problem_2/sphere.py
@@ -1,23 +1,24 @@
 import glob
 import cv2
-import regex as re
 import numpy as np
-from .lucas_kanade import lucas_kanade
+import regex as re
+from .lk_pyramid import lucas_kanade_pyramid
 from .interpolations import warp_flow
+from.parameters import *
 
 
 def sphere_interpolation(N=11):
     images = glob.glob('./input/sphere/*.ppm')
     images.sort(key=lambda f: int(re.sub('\D', '', f)))
 
-    for ind in range(0, len(images) - 2, 2):
+    for ind in range(0, len(images) - 1, 2):
         firstImage = cv2.imread(images[ind], flags=cv2.IMREAD_GRAYSCALE)
         secondImage = cv2.imread(images[ind + 2], flags=cv2.IMREAD_GRAYSCALE)
 
         firstImage = np.array(firstImage, dtype=np.float32)
         secondImage = np.array(secondImage, dtype=np.float32)
 
-        forward_flow, If = lucas_kanade(firstImage, secondImage, N, ind, 'sphere')
-        backward_flow, Ib = lucas_kanade(secondImage, firstImage, N, ind, 'sphere')
+        forward_flow, If = lucas_kanade_pyramid(firstImage, secondImage, ITERATION, LEVEL)
+        backward_flow, Ib = lucas_kanade_pyramid(secondImage, firstImage, ITERATION, LEVEL)
 
         warp_flow(firstImage, secondImage, forward_flow, If, backward_flow, Ib, ind, 'sphere')
